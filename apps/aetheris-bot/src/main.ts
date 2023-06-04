301c8770c55e5d90a2272e6d5c30e36e1b41f7b5
yehezkieldio <yehezkieldio35@gmail.com>
2023-06-05 07:33:09 +0800

    feat!: add plenty of things

---
+++ apps/aetheris-bot/src/main.ts
@@ -0,0 +1,6 @@
+import { SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits } from 'discord.js';
+
+const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages], baseUserDirectory: __dirname, loadMessageCommandListeners: true });
+client.login(process.env.DISCORD_TOKEN);
+

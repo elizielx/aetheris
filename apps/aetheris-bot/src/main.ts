30f629f1409949a37b874f0b7711c40152fc07f6
yehezkieldio <yehezkieldio35@gmail.com>
2023-06-06 04:15:07 +0800

    chore(aetheris-bot): add environment variable handling for Discord bot token

--- apps/aetheris-bot/src/main.ts
+++ apps/aetheris-bot/src/main.ts
@@ -1,6 +1,9 @@
-import { SapphireClient } from '@sapphire/framework';
-import { GatewayIntentBits } from 'discord.js';
+import { SapphireClient } from "@sapphire/framework";
+import { GatewayIntentBits } from "discord.js";
 
-const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages], baseUserDirectory: __dirname, loadMessageCommandListeners: true });
-client.login(process.env.DISCORD_TOKEN);
-
+const client = new SapphireClient({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+    baseUserDirectory: __dirname,
+    loadMessageCommandListeners: true,
+});
+client.login(process.env.NODE_ENV === "production" ? process.env.DISCORD_TOKEN : process.env.CANARY_DISCORD_TOKEN);

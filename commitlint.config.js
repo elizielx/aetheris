244beef0ab23c60b1ea3aa0569a08647e58ec6ed
yehezkieldio <yehezkieldio35@gmail.com>
2023-06-06 20:07:49 +0800

    refactor(aetheris-api): remove prisma from api

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "scope-enum": [2, "always", ["aetheris"]],
+        "scope-enum": [2, "always", ["aetheris-api", "aetheris-web", "aetheris-bot", "aetheris-lib"]],
         "type-enum": [
             2,
             "always",
